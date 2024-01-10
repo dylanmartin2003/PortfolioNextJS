7646c02458f58e6bea2c19cd8b3389886cde423a
Dylan Martin <martindylan0406@gmail.com>
2024-01-10 15:55:11 +0100

    Modification style du site

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/**@type {import('next').NextConfig}*/
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX;
+
+const config = {
+    output: 'export',
+    trailingSlash: false,
+    distDir: 'out',
+    reactStrictMode: true,
+    assetPrefix,
+    basePath,
+    images: {
+        unoptimized: true,
+    }
+};
+
+export default config;
