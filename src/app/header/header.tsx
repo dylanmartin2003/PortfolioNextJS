04a1c28e852a0335c899dc2e0235869087f87ca4
Dylan Martin <martindylan0406@gmail.com>
2024-01-10 16:27:09 +0100

    Modification style du site

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,7 +1,7 @@
 export default function header() {
     return (
         <header className="flex items-center justify-between px-4 py-6 md:px-8 lg:px-12 bg-cyan-500">
-            <a className="flex items-center" href="/">
+            <a className="flex items-center" href="/PortfolioNextJS">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
