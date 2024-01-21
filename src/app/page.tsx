2e37f9dd8511939811fa350c307f10fe1e8b7b38
MARTIN Dylan <martindylan0406@gmail.com>
2024-01-21 21:45:36 +0100

    Modification portfolio, ajout data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import Header from "@/app/header/header";
 import Image from "next/image";
 
 export default function Home() {
+
+    const texte = "Je m'appelle Martin Dylan, j'ai 20 ans et je suis passionné par l'informatique. Actuellement, je suis en alternance chez Lenrek Informatique, où je m'immerge dans le monde captivant des technologies et des solutions informatiques.\n" +
+        "                        Mon parcours dans le domaine informatique a été une aventure stimulante, m'offrant des opportunités d'apprentissage constant. Mon intérêt pour l'informatique va au-delà des simples lignes de code ; j'aime explorer les nouvelles tendances technologiques, résoudre des problèmes complexes et contribuer à des projets innovants.\n" +
+        "                        En tant que jeune professionnel, je suis constamment en quête de nouvelles connaissances et d'expériences enrichissantes. Travailler en alternance chez Lenrek Informatique m'offre l'occasion idéale d'appliquer mes compétences tout en apprenant aux côtés d'experts du secteur."
+
   return (
     <main>
         <div>
@@ -14,9 +19,7 @@ export default function Home() {
                 </div>
                 <div className="w-full lg:w-1/2 p-4">
                     <h2 className="text-2xl font-bold mb-4">MARTIN Dylan</h2>
-                    <p>Je m'appelle Martin Dylan, j'ai 20 ans et je suis passionné par l'informatique. Actuellement, je suis en alternance chez Lenrek Informatique, où je m'immerge dans le monde captivant des technologies et des solutions informatiques.
-                        Mon parcours dans le domaine informatique a été une aventure stimulante, m'offrant des opportunités d'apprentissage constant. Mon intérêt pour l'informatique va au-delà des simples lignes de code ; j'aime explorer les nouvelles tendances technologiques, résoudre des problèmes complexes et contribuer à des projets innovants.
-                        En tant que jeune professionnel, je suis constamment en quête de nouvelles connaissances et d'expériences enrichissantes. Travailler en alternance chez Lenrek Informatique m'offre l'occasion idéale d'appliquer mes compétences tout en apprenant aux côtés d'experts du secteur.</p>
+                    <p>{texte}</p>
                 </div>
             </div>
         </div>
